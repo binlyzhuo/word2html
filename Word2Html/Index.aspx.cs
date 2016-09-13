2347e50e7acacf23b8d5d483351cc5e043eee78b
binlyzhuo <binlyzhuo@outlook.com>
2016-09-13 09:13:17 +0800

    ~commit

---
+++ Word2Html/Index.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using HtmlAgilityPack;
+using Aspose.Words;
+
+
+namespace Word2Html
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
