21076f43b8465a8b10cd0c41a37f0f3cf5a7d456
binlyzhuo <binlyzhuo@outlook.com>
2016-09-14 10:18:02 +0800

    ~commit

--- Word2Html/Index.aspx
+++ Word2Html/Index.aspx
@@ -10,6 +10,15 @@
     <form id="form1" runat="server">
     <div>
     
+        <asp:FileUpload ID="FileUpload1" runat="server" />
+        <br />
+        <br />
+        <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Upload" Width="83px" />
+        <br />
+        <br />
+        <asp:HyperLink ID="HyperLink1" runat="server">HyperLink</asp:HyperLink>
+        <br />
+    
     </div>
     </form>
 </body>
--- Word2Html/Index.aspx.cs
+++ Word2Html/Index.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,8 +13,35 @@ namespace Word2Html
 {
     public partial class Index : System.Web.UI.Page
     {
+        private string saveDoc = "D:\\mydocs";
         protected void Page_Load(object sender, EventArgs e)
         {
+            string wordFile = "";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string fileName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            FileUpload1.SaveAs(string.Format(@"{0}\\{1}.doc",saveDoc,fileName));
+
+            Aspose.Words.Document d = new Aspose.Words.Document(string.Format(@"{0}\\{1}.doc", saveDoc, fileName));
+            d.Save(string.Format(@"{0}\\{1}.html", saveDoc, fileName), SaveFormat.Html);
+
+
+            HyperLink1.NavigateUrl = string.Format("/HtmlPage.aspx?f={0}",fileName);
+
+            HtmlDocument document = new HtmlDocument();
+            document.Load(string.Format(@"{0}\\{1}.html", saveDoc, fileName),Encoding.UTF8);
+
+            var docNode= document.DocumentNode.SelectNodes("/html/body/div/p");
+
+
+            foreach (var dc in docNode)
+            {
+                Response.Write(dc.InnerText);
+                Response.Write("<br/>");
+            }
 
         }
     }
